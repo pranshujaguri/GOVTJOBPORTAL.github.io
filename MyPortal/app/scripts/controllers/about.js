a94801c41969be61f850f7a11f23e484edea5865
pranshujaguri <pranshujaguri@gmail.com>
2017-05-17 11:39:21 +0530

    codes

---
+++ MyPortal/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myPortalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myPortalApp
+ */
+angular.module('myPortalApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ MyPortal/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myPortalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myPortalApp
+ */
+angular.module('myPortalApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
